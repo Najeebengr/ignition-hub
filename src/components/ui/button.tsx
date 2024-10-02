90c4d6c88f0da311e800745438cd42cb63c502ed
Najeeb Ur Rehman <najeeb.nedo5555@gmail.com>
2024-10-02 20:57:26 +0500

    check

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,7 @@
 import { buttonType } from '@/types'
 import React from 'react'
 
-function Button({variant, text, icon}: buttonType) {
+function Button({ text, icon}: buttonType) {
   return (
     <button className={`bg-secondary flex items-center gap-2 text-white px-5 py-2 rounded-md`}>
         {text}
